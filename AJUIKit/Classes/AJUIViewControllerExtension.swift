cace1527742400cb2cafcc72818361e6e4c650c7
shanying.xu <shanying.xu@tuya.com>
2022-10-16 15:02:30 +0800

    feat: update

--- AJUIKit/Classes/AJUIViewControllerExtension.swift
+++ AJUIKit/Classes/AJUIViewControllerExtension.swift
@@ -6,27 +6,72 @@
 //
 
 import UIKit
+import AJFoundation
 
 public extension UIViewController {
     class func currentViewController() -> UIViewController {
         var viewController = UIViewController.init()
-        let vc = UIApplication.shared.delegate?.window!?.rootViewController
-//        let vc = UIApplication.shared.keyWindow!.rootViewController!
+        guard let delegate = UIApplication.shared.delegate else {
+            return viewController
+        }
+        guard let window = delegate.window else {
+            return viewController
+        }
+        guard let vc = window?.rootViewController else {
+            return viewController
+        }
+        return vc.currentViewController()
+    }
+    
+    func currentViewController() -> UIViewController {
+        let vc = self
+        AJLog("rootViewController:\(NSStringFromClass(type(of: vc)))")
+        var viewController = UIViewController()
         if vc is UITabBarController {
             let tabbar = vc as! UITabBarController
-            viewController = tabbar.selectedViewController!
+            viewController = getTabBarSelectedVC(tabbar: tabbar)
         } else if vc is UINavigationController {
             let nav = vc as! UINavigationController
-            viewController = nav.visibleViewController!
+            viewController = getNavigationVisibleVC(nav: nav)
         } else {
-            viewController = vc!
+            viewController = vc
+        }
+        AJLog("currentVC:\(NSStringFromClass(type(of: viewController)))")
+        return viewController;
+    }
+    
+    private func getNavigationVisibleVC(nav: UINavigationController) -> UIViewController {
+//        AJLog("navigationController:\(NSStringFromClass(type(of: nav)))")
+        var viewController = UIViewController()
+        guard let visibleVC = nav.visibleViewController else {
+            return viewController
         }
         
-        if viewController is UINavigationController {
-            let nav = viewController as! UINavigationController
-            viewController = nav.visibleViewController!
+        if visibleVC is UITabBarController {
+            let tabbar = visibleVC as! UITabBarController
+            viewController = getTabBarSelectedVC(tabbar: tabbar)
+        } else {
+//            AJLog("visibleVC:\(NSStringFromClass(type(of: visibleVC)))")
+            viewController = visibleVC
         }
-        return viewController;
+        return viewController
+    }
+    
+    private func getTabBarSelectedVC(tabbar: UITabBarController) -> UIViewController {
+//        AJLog("tabBarController:\(NSStringFromClass(type(of: tabbar)))")
+        var viewController = UIViewController()
+        guard let selectedVC = tabbar.selectedViewController else {
+            return viewController
+        }
+        if selectedVC is UINavigationController {
+//            AJLog("selectedNav:\(NSStringFromClass(type(of: selectedVC)))")
+            let nav = selectedVC as! UINavigationController
+            viewController = getNavigationVisibleVC(nav: nav)
+        } else {
+//            AJLog("selectedVC:\(NSStringFromClass(type(of: selectedVC)))")
+            viewController = selectedVC
+        }
+        return viewController
     }
 }
 
